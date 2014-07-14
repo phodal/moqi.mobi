ee8b0d475c6331f4e09f553211a1e3f57abfa4cd
Phodal Huang <h@phodal.com>
2014-07-14 22:27:11 +0800

    add the mustache module

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 // Filename: app.js
-define(['jquery', 'underscore'], function($, _){
+define(['jquery', 'underscore', 'mustache'], function($, _, Mustache){
     var initialize = function() {
-        console.log("init test");
+        var person = {
+            firstName: "Phodal",
+            lastName: "Huang",
+            blogURL: "http://www.phodal.com"
+        };
+        var template = "<h1>{{firstName}} {{lastName}}</h1>Blog: {{blogURL}}";
+        var html = Mustache.to_html(template, person);
+        $('#sampleArea').html(html);
     }
 
     return {
--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 require.config({
     baseUrl: 'lib/',
     paths: {
-        // the left side is the module ID,
-        // the right side is the path to
-        // the jQuery file, relative to baseUrl.
-        // Also, the path should NOT include
-        // the '.js' file extension. This example
-        // is using jQuery 1.9.0 located at
-        // js/lib/jquery-1.9.0.js, relative to
-        // the HTML page.
-        jquery: 'jquery-2.1.1.min'
+        jquery: 'jquery-2.1.1.min',
+        'templates': '../templates'
     },
     shim: {
         underscore: {
@@ -19,6 +12,5 @@ require.config({
 });
 
 require(['../js/app'], function(App){
-    // The "app" dependency is passed in as "App"
     App.initialize();
 });
