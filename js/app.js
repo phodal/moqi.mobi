587c4a2bf95859e20c2c251a162c6a8b2bedf19d
Phodal Huang <h@phodal.com>
2014-07-15 20:16:24 +0800

    move the seo information to js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Filename: app.js
-define(['jquery', 'underscore', 'mustache', "text!/templates/nav.html", "json!http://api.phodal.net/blog/page/1"], function($, _, Mustache, navTemplate, blogPosts){
+define(['jquery', 'underscore', 'mustache', "text!/templates/nav.html", "text!/index.html", "json!http://api.phodal.net/blog/page/1"],
+    function($, _, Mustache, navTemplate, indexTemplate,blogPosts){
 
     var initialize = function() {
-        var template = navTemplate;
+        this.template = navTemplate;
+        this.indexTemplate=indexTemplate;
 
         var info = [];
 
@@ -18,8 +20,15 @@ define(['jquery', 'underscore', 'mustache', "text!/templates/nav.html", "json!ht
 
         console.log(info);
 
-        var html = Mustache.to_html(template, info);
+        var html = Mustache.to_html(this.template, info);
         $('#blogArea').html(html);
+
+        var seoinfo ={
+            description:"墨颀移动 CMS移动平台的CMS解决方案",
+            keywords:"移动CMS,移动CMS框架,移动平台CMS,移动CMS系统",
+            title:"墨颀 CMS - 移动平台的CMS解决方案"
+        };
+        $('head').html(Mustache.to_html(this.indexTemplate, seoinfo));
     };
 
     return {
